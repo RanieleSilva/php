417e00bdfed2585eaa1b1a6917b540f5a8e0e732
RanieleSilva <ranieledasilvasabino@gmail.com>
2017-05-03 15:22:37 -0300

    Add files via upload

---
+++ ex11.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Exemplo 11</title>
+</head>
+<body bgcolor=#847281>
+<center>
+<h1>Estrutura de Repetição DO WHILE</h1>
+	<?php
+ $num=10;
+ do{
+ 	echo "Número: $num<br>";
+ 	$num--;
+ } while ( $num>=1);
+?>
+</body>
+</html>
