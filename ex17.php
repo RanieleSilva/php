c6ae0d60cf3c68a25394548fbeb98f76af4b1f00
RanieleSilva <ranieledasilvasabino@gmail.com>
2017-05-09 15:43:42 -0300

    Add files via upload

---
+++ ex17.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+</head>
+<body>
+<h1>"<b>exemplo 17"</h1>
+<?php
+echo"<h1> lendo um vetor</h1>"
+$frutas=array("laranja","maça","uva","banana","mamão");
+foreach($frutas as $valor){
+echo"<li>fruta:$valor<li>";
+}
+?>
+</body>
+</html>
+
